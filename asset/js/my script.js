bd24019ac8ae4ab5d35b6f583cb2b3c74b1194cd
Reyhanehghanei <regh9108@gmail.com>
2023-04-06 05:24:20 -0500

    dfd

--- asset/js/my script.js
+++ asset/js/my script.js
@@ -11,3 +11,15 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
+    var account_btn = document.getElementById("account-btn")
+    var login = document.getElementById("login")
+    var register = document.getElementById("register")
+
+    function Register() {
+      account_btn.style.left="110px";
+      register.style.left="50px";
+      login.style.left="-400px";
+    }
